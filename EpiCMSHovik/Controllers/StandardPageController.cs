c288e706d6cbb3cf670cd4f79bf7a7a85971f0de
Hovik Melkomian <hovik.melkomian@luminoslabs.com>
2019-02-13 10:39:29 -0800

    B3

---
+++ EpiCMSHovik/Controllers/StandardPageController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EpiCMSHovik.Models.Pages;
+using EpiCMSHovik.Models.ViewModels;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+
+namespace EpiCMSHovik.Controllers
+{
+    public class StandardPageController : PageControllerBase<StandardPage>
+    {
+        public ActionResult Index(StandardPage currentPage)
+        {
+            /* Implementation of action. You can create your own view model class that you pass to the view or
+             * you can pass the page type for simpler templates */
+            DefaultPageViewModel<StandardPage> pageViewModel = new DefaultPageViewModel<StandardPage>(currentPage);
+
+            return View(pageViewModel);
+        }
+    }
+}
---
+++ EpiCMSHovik/Models/Pages/StandardPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace EpiCMSHovik.Models.Pages
+{
+    [ContentType(DisplayName = "StandardPage", GUID = "fa933b2b-d770-4389-80f3-c82d47d19625", Description = "Alloy Standard Page")]
+    public class StandardPage : SitePageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 310)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
