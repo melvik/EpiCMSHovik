e31f8c6ea35916bc5313ea1d868ad2313ddc98ab
Hovik Melkomian <hovik.melkomian@luminoslabs.com>
2019-02-13 20:24:42 -0800

    F1 Search has problem

---
+++ EpiCMSHovik/Models/Pages/SearchPage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace EpiCMSHovik.Models.Pages
+{
+    [ContentType(DisplayName = "SearchPage", GUID = "ce08a26e-d72f-4281-9ae5-4cf9d7ca9379", Description = "Searching Page")]
+    public class SearchPage : SitePageData
+    {
+    }
+}
---
+++ EpiCMSHovik/Models/ViewModels/SearchPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EpiCMSHovik.Models.Pages;
+using EPiServer.Globalization;
+using Lucene.Net.Search.Spans;
+
+namespace EpiCMSHovik.Models.ViewModels
+{
+    public class SearchPageViewModel : IPageViewModel<SearchPage>
+    {
+        public SearchPage CurrentPage { get; }
+        public string SearchText { get; set; }
+        //public List<IndexResponseItem> SearchResult { get; set; }
+
+        public SearchPageViewModel(SearchPage currentPage)
+        {
+            CurrentPage = currentPage;
+        }
+
+        public void Search(string q)
+        {
+            var cultureSet = ContentLanguage.PreferredCulture.Name;
+            //SearchResult = new List<IndexResponseItem>();
+            //var fieldQueryResult = SearchHandler.
+        }
+    }
+}
