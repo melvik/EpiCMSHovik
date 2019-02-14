b9330a4528bbc49f2efe9fb17e88b2a9bc9eb5a7
Hovik Melkomian <hovik.melkomian@luminoslabs.com>
2019-02-13 17:08:15 -0800

    finish D2

---
+++ EpiCMSHovik/Controllers/TeaserBlockController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EpiCMSHovik.Models.Blocks;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+
+namespace EpiCMSHovik.Controllers
+{
+    public class TeaserBlockController : BlockController<TeaserBlock>
+    {
+        public override ActionResult Index(TeaserBlock currentBlock)
+        {
+            return PartialView(currentBlock);
+        }
+    }
+}
---
+++ EpiCMSHovik/Models/Blocks/EditorialBlock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace EpiCMSHovik.Models.Blocks
+{
+    [ContentType(DisplayName = "EditorialBlock", GUID = "6cd61d36-7ba6-4846-b071-dad3a72ef42e", Description = "block 1")]
+    public class EditorialBlock : BlockData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Main Body",
+            Description = "Name field's description",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
